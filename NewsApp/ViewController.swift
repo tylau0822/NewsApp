051b23f3779c5ea0431592051adf8bf9498095fb
tylau0822 <34152633+tylau0822@users.noreply.github.com>
2024-05-30 20:45:50 -0400

    Initial commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by LAU TSZ YING on 30/5/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
